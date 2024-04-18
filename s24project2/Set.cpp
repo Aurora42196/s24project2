63d43388bc017e74f5b08e0e85cd9d27673d3e70
Cameron Maiden <cmaiden42@g.ucla.edu>
2024-04-18 16:42:13 -0700

    Created the appropriate source and header files

---
+++ s24project2/Set.cpp
@@ -0,0 +1,8 @@
+//
+//  Set.cpp
+//  s24project2
+//
+//  Created by Cameron Maiden on 4/18/24.
+//
+
+#include "Set.h"
---
+++ s24project2/Set.h
@@ -0,0 +1,13 @@
+//
+//  Set.h
+//  s24project2
+//
+//  Created by Cameron Maiden on 4/18/24.
+//
+
+#ifndef Set_h
+#define Set_h
+
+#include <stdio.h>
+
+#endif /* Set_h */

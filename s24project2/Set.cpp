7fb8cad33ead847bdc60f9ecff06156663034817
Cameron Maiden <cmaiden42@g.ucla.edu>
2024-04-22 01:26:33 -0700

    *Implemented the default constructor with a dummy node *Added member function implementations in Set.cpp enough to allow the project to compile *Added Non-member function declarations and implementations enough to allow the project to compile *Added struct Node as a private member of the Set class *Added comments to organize and describe the use of each funcion of the Set class

--- s24project2/Set.cpp
+++ s24project2/Set.cpp
@@ -8,10 +8,84 @@
 #include "Set.h"
 
 // Default constructor => create an empty Set
+/// The constructor creates an empty linked list starting with a dummy node
 Set::Set()
  :m_size(0)
+{
+    head = new Node;
+    head->next = head;
+    head->prev = head;
+}
+
+bool Set::empty() const // Checking to verify if the list is empty
+{
+    return (m_size == 0);
+}
+
+int Set::size() const // Simply returns the size of the list
+{
+    return m_size;
+}
+
+bool Set::insert(const ItemType &value) // inserts a new value in the list in sorted order
+{
+    return false;
+}
+
+bool Set::erase(const ItemType &value) // deletes the passed in value assuming it exists within the list
+{
+    return false;
+}
+
+bool Set::contains(const ItemType &value) const // simply checks if a value already exists within the list
+{
+    return false;
+}
+
+bool Set::get(int pos, ItemType &value) const // retrieves the value in the list that's exactly pos items less than the one in the list
+{
+    return false;
+}
+
+void Set::swap(Set &other) // swaps the contents of two lists
 {
     
 }
 
+///////////////////////////////////////////////////////////////////////////
+/// Housekeeping Functions
+///////////////////////////////////////////////////////////////////////////
 
+Set::~Set() // Destructor
+{
+    
+}
+
+Set::Set(const Set& other) // Copy Constructor
+{
+    
+}
+
+Set& Set::operator=(const Set& rhs) // Assignment operator
+{
+    return *this;
+}
+
+void Set::dump() const
+{
+    
+}
+
+///////////////////////////////////////////////////////////////////////////
+/// Non-member Functions
+///////////////////////////////////////////////////////////////////////////
+
+void unite(const Set& s1, const Set& s2, Set& result)
+{
+    
+}
+
+void inOnlyOne(const Set& s1, const Set& s2, Set& result)
+{
+    
+}
--- s24project2/Set.h
+++ s24project2/Set.h
@@ -8,7 +8,7 @@
 #ifndef Set_h
 #define Set_h
 
-#include <iostream>
+#include <string>
 
 using ItemType = std::string;
 
@@ -25,14 +25,39 @@ public:
     void swap(Set& other);
     
     // Housekeeping Functions
-    ~Set(); //When a Set is destroyed, the nodes in the linked list must be deallocated.
+    ~Set(); // destructor 
+    ///When a Set is destroyed, the nodes in the linked list must be deallocated.
     
-    Set(const Set& other); // When a brand new Set is created as a copy of an existing Set, enough new nodes must be allocated to hold a duplicate of the original list.
+    Set(const Set& other); // copy constructor 
+    /// When a brand new Set is created as a copy of an existing Set, enough new nodes 
+    /// must be allocated to hold a duplicate of the original list.
 
-    Set& operator=(const Set& rhs); // When an existing Set (the left-hand side) is assigned the value of another Set (the right-hand side), the result must be that the left-hand side object is a duplicate of the right-hand side object, with no memory leak of list nodes (i.e. no list node from the old value of the left-hand side should be still allocated yet inaccessible).
+    Set& operator=(const Set& rhs); // assignment operator overload
+    /// When an existing Set (the left-hand side) is assigned the value of another Set (the right-hand side),
+    /// the result must be that the left-hand side object is a duplicate of the right-hand side object, with no
+    /// memory leak of list nodes (i.e. no list node from the old value of the left-hand side should be still
+    /// allocated yet inaccessible).
+    
+    void dump() const;
     
 private:
     int m_size; // holds the number of how many items are in the linked list
+    
+    struct Node
+    {
+        ItemType data;
+        Node* next;
+        Node* prev;
+    };
+    
+    Node* head = nullptr;
 };
+
+///////////////////////////////////////////////////////////////////////////
+/// Non-member Functions
+///////////////////////////////////////////////////////////////////////////
+
+void unite(const Set& s1, const Set& s2, Set& result);
+void inOnlyOne(const Set& s1, const Set& s2, Set& result);
 
 #endif /* Set_h */

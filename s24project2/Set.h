40c1e2bf04d450bce977ca7bda096dee765127b9
Cameron Maiden <cmaiden42@g.ucla.edu>
2024-04-21 21:36:36 -0700

    *Added Set class function declarations to Set.h

--- s24project2/Set.h
+++ s24project2/Set.h
@@ -8,6 +8,29 @@
 #ifndef Set_h
 #define Set_h
 
-#include <stdio.h>
+#include <iostream>
+
+using ItemType = std::string;
+
+class Set
+{
+public:
+    Set();
+    bool empty() const;
+    int size() const;
+    bool insert(const ItemType& value);
+    bool erase(const ItemType& value);
+    bool contains(const ItemType& value) const;
+    bool get(int pos, ItemType& value) const;
+    void swap(Set& other);
+    
+    // Housekeeping Functions
+    ~Set();
+    Set(const Set& other);
+    Set& operator=(const Set& rhs);
+    
+private:
+    
+};
 
 #endif /* Set_h */

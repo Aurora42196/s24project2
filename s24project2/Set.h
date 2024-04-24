c39601a7da3148fb617f201deeb56541be8d3237
Cameron Maiden <cmaiden42@g.ucla.edu>
2024-04-23 22:23:48 -0700

    *grouped memory leak test files into its own folder/group

--- s24project2/Set.h
+++ s24project2/Set.h
@@ -9,13 +9,13 @@
 #define Set_h
 
 #include <string>
+///////////// Include the two lines below to test for memory leaks
+//#include "IntWrapper.h"      // TODO: remove this line after testing
+//using ItemType = IntWrapper; // TODO: replace this line after testing
 
-//using ItemType = std::string;
+using ItemType = std::string;
 //using ItemType = unsigned long;
 
-///////////// Included to test for memory leaks
-#include "IntWrapper.h"      // TODO: remove this line after testing
-using ItemType = IntWrapper; // TODO: replace this line after testing
 
 
 class Set
